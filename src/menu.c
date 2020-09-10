d9bbaf102c583771ab1332eb242608de058ba61b
Valeria Karon <vkaron@il-g1.msk.21-school.ru>
2020-09-10 16:41:49 +0300

    norm menu

--- src/menu.c
+++ src/menu.c
@@ -6,7 +6,7 @@
 /*   By: vkaron <vkaron@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 14:24:16 by vkaron            #+#    #+#             */
-/*   Updated: 2020/09/10 16:19:55 by vkaron           ###   ########.fr       */
+/*   Updated: 2020/09/10 16:39:08 by vkaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 void	print_wolf(SDL_Surface *sdest, const char *text,
 	SDL_Rect *dest)
 {
+	const char	*f = "prgres/wf.otf";
 	SDL_Color	col;
 	TTF_Font	*fnt;
 	SDL_Surface	*stext;
-	const char	*f = "prgres/wf.otf";
 
 	col = (SDL_Color){171, 32, 46, 255};
 	fnt = NULL;
@@ -40,23 +40,19 @@ void	draw_menu(t_game *game)
 	SDL_Point	pos;
 	int			index;
 	SDL_Rect	r;
-	int flag = 1;
-	
+
 	pos.y = -1;
 	while (++pos.y < S_H)
 	{
 		index = pos.y * S_W;
 		pos.x = -1;
-		while(++pos.x < S_W)
+		while (++pos.x < S_W)
 		{
 			game->data[index + pos.x] = game->data_menu[index + pos.x];
 		}
 	}
 	r = (SDL_Rect){H_W + H_W / 2, H_H - 20, 0, 0};
-	if (flag)
-		print_wolf(game->surf, "Continue", &r);
-	else
-		print_wolf(game->surf, "", &r);
+	print_wolf(game->surf, "Continue", &r);
 	print_wolf(game->surf, "New game", &r);
 	print_wolf(game->surf, "Settings", &r);
 	print_wolf(game->surf, "Editor", &r);
@@ -67,7 +63,7 @@ void	main_menu(t_game *game)
 {
 	SDL_Point	flags;
 	SDL_Event	e;
-	SDL_Rect r;
+	SDL_Rect	r;
 
 	flags.x = 0;
 	flags.y = 1;
@@ -75,26 +71,18 @@ void	main_menu(t_game *game)
 	r.w = 100;
 	r.x = 0;
 	r.y = 0;
-	//init_editor(&ed);
 	while (!flags.x)
 	{
 		if (SDL_PollEvent(&e) != 0)
 			sld_events_menu(game, e, &flags);
 		if (flags.y)
 		{
-			//SDL_FillRect(game->menu, NULL, 0xFFFF0000);
-            
-			//draw_map_editor(game, &ed);
-			//draw_col_frame(game, &ed, ed.cursor.pos, 0xFFFF0000);
-			//draw_menu(game, &ed);
-			//status_selector(game, &ed);
-            //SDL_BlitSurface(game->menu, NULL, game->surf, NULL);
 			draw_menu(game);
 			SDL_UpdateWindowSurface(game->win);
 			flags.y = 0;
 		}
 		SDL_Delay(5);
 	}
-    if (flags.x == 1)
-	    game->status = 0;
+	if (flags.x == 1)
+		game->status = 0;
 }
